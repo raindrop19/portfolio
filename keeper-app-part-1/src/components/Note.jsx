cbbce91ad8b546c9a2cc2e7aaf0ef9cecdd9076a
raindrop19 <58416409+raindrop19@users.noreply.github.com>
2019-12-10 14:03:14 +0800

    Add files via upload

---
+++ keeper-app-part-1/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Udemy WebDev</h1>
+      <p>Study react.</p>
+    </div>
+  );
+}
+
+export default Note;
